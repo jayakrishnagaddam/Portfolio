4709725a9e252fc736255676dfbf0909c3f5e92e
jayakrishnagaddam <115868714+jayakrishnagaddam@users.noreply.github.com>
2024-05-08 17:09:40 +0530

    Added projects

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ app=Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
